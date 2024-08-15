ffd3042ecf1082a5ccdc7a60e43d64a7568f070a
Boniphace Tech <167980803+Boniphace1@users.noreply.github.com>
2024-08-16 02:08:51 +0300

    Update mesfonctions.js

--- framework/mesfonctions.js
+++ framework/mesfonctions.js
@@ -32,7 +32,7 @@ module.exports.genererNomFichier = async (extension) => {
 /** ************ */
 module.exports.stick = async (buffer, author) => {
     var sticker = new Sticker(buffer, {
-        pack: 'BONIPHACE-MD',
+        pack: 'PHOTOGRAPHER-MD',
         author: author,
         type: StickerTypes.FULL,
         categories: ['🤩', '🎉'],
