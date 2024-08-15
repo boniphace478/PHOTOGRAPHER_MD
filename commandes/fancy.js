59be47d4f6c11c43ec997f053020ea190a26bb71
FredieTech <frediezra60@gmail.com>
2024-08-16 00:58:46 +0300

    Update fancy.js

--- commandes/fancy.js
+++ commandes/fancy.js
@@ -8,7 +8,7 @@ zokou({ nomCom: "fancy", categorie: "Fun", reaction: "☑️" }, async (dest, zk
 
     try {
         if (id === undefined || text === undefined) {
-            return await repondre(`\nExemple : ${prefixe}fancy 10 boniphace tech\n` + String.fromCharCode(8206).repeat(4001) + fancy.list('BONIPHACE-MD', fancy));
+            return await repondre(`\nExemple : ${prefixe}fancy 10 photographer tech\n` + String.fromCharCode(8206).repeat(4001) + fancy.list('XPHOTOGRAPHER-MD', fancy));
         }
 
         const selectedStyle = fancy[parseInt(id) - 1];
